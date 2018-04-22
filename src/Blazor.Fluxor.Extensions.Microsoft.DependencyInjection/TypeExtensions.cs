aaf856d492a417f90a735ce02cc83140e7655f2e
X <X>
2018-04-22 19:38:10 +0100

    Auto-discover features

---
+++ src/Blazor.Fluxor.Extensions.Microsoft.DependencyInjection/TypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Blazor.Fluxor
+{
+	internal static class TypeExtensions
+	{
+		internal static Type[] GetGenericParametersForSpecificGenericType(Type implementingType, Type requiredGenericType)
+		{
+			while (implementingType != null)
+			{
+				if (implementingType.IsGenericType && implementingType.GetGenericTypeDefinition() == requiredGenericType)
+					return implementingType.GetGenericArguments();
+				implementingType = implementingType.BaseType;
+			}
+			return null;
+		}
+
+	}
+}
